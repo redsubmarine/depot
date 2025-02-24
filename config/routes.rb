694e3cede92701e8994f4b623936b58317ca43b4
red <wonseok@duck.com>
2025-02-24 23:22:34 +0900

    home 화면 레이아웃 적용.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get "store/index"
+  root "store#index", as: "store_index"
   resources :products
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
