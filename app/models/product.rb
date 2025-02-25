67abba817d7d2bab7791a2bc05ee9eda89f52f25
red <wonseok@duck.com>
2025-02-25 21:46:50 +0900

    line item with testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   has_one_attached :image
 
   after_commit -> { broadcast_refresh_later_to "products" }
@@ -17,4 +20,12 @@ class Product < ApplicationRecord
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  private
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
+    end
+  end
 end
