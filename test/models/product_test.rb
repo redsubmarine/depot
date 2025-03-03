221fa828fec5a087126edb33ab83c5c6fe25ad0e
red <wonseok@duck.com>
2025-03-03 22:45:09 +0900

    fix lint

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -17,11 +17,11 @@ class ProductTest < ActiveSupport::TestCase
     product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
     product.price = BigDecimal(-1)
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
 
     product.price = BigDecimal(0)
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
 
     product.price = BigDecimal(1)
     assert product.valid?
@@ -42,7 +42,7 @@ class ProductTest < ActiveSupport::TestCase
     product.image.attach(io: File.open("test/fixtures/files/lorem.jpg"), filename: "lorem.jpg", content_type: "image/jpeg")
 
     assert product.invalid?
-    assert_equal ["has already been taken"], product.errors[:title]
+    assert_equal [ "has already been taken" ], product.errors[:title]
     # assert_equal [I18n.translate("errors.messages.taken")], product.errors[:title]
   end
 end
