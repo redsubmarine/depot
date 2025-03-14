7f5b4b2e47b57f0290c965a90b94ee5b3a21220d
Wonseok Yang <wsyang@digicaps.com>
2025-03-14 16:11:43 +0900

    피커에서 선택된 내용으로 화면 갱신 처리.

--- app/javascript/controllers/payment_controller.js
+++ app/javascript/controllers/payment_controller.js
@@ -1,7 +1,18 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="payment"
 export default class extends Controller {
-  connect() {
-  }
+    static targets = ["selection", "additionalFields"]
+
+    initialize() {
+        this.showAdditionalFields()
+    }
+
+    showAdditionalFields() {
+        const selection = this.selectionTarget.value
+        for (let fields of this.additionalFieldsTargets) {
+            fields.disabled = fields.hidden = (fields.dataset.type !== selection)
+        }
+    }
+
 }
